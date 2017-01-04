4b5c564fe532316bdc7dba78dee8a5a2dcc059f4
Caitlyn Chen <caitlynachen@gmail.com>
2017-01-03 17:17:09 -0800

    keyboard and comment work

--- CommentViewController.swift
+++ CommentViewController.swift
@@ -21,6 +21,7 @@ class CommentViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     var comments: [CommentObject] = []
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,7 +46,17 @@ class CommentViewController: UIViewController, UITableViewDelegate, UITableViewD
             self.tableView.reloadData()
         })
 
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardNotification(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
 
+
+        let tap : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        
+        view.addGestureRecognizer(tap)
+
+    }
+    
+    func dismissKeyboard(){
+        view.endEditing(true)
     }
     
     @IBAction func sendButtonTapped(_ sender: Any) {
@@ -73,6 +84,25 @@ class CommentViewController: UIViewController, UITableViewDelegate, UITableViewD
         return cell
     }
 
+    func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let duration:TimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIViewAnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if (endFrame?.origin.y)! >= UIScreen.main.bounds.size.height {
+                self.bottomConstraint?.constant = 20.0
+            } else {
+                self.bottomConstraint?.constant = endFrame?.size.height ?? 30.0
+            }
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: { self.view.layoutIfNeeded() },
+                           completion: nil)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
